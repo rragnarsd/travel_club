0762cbd8eb94f668d71975ae7e9240cb85b3dc89
Regina Ragnarsdottir <reginaragnarsd@gmail.com>
2021-05-15 13:14:55 +0000

    first commit

---
+++ js.js
@@ -0,0 +1,12 @@
+/*Jquery*/
+
+$(document).ready(function(){
+
+    $nav = $('.nav');
+    $toggleCollapse = $('.toggle-collapse');
+
+    /*click event on toggle menu*/
+    $toggleCollapse.click(function() {
+        $nav.toggleClass('collapse');
+    })
+})
